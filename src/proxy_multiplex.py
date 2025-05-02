8e3a9ac8c422fd8fc94e501b45b45acce3177ea0
thw-jiaolab <thw@berkeley.edu>
2025-05-01 19:56:09 -0700

    use one session for all target ws

--- src/proxy_multiplex.py
+++ src/proxy_multiplex.py
@@ -6,7 +6,7 @@ import logging
 import os
 import time
 from contextlib import asynccontextmanager
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import aiohttp
 from fastapi.websockets import WebSocketState
@@ -21,7 +21,10 @@ import uvloop
 
 logging.basicConfig(
     level=logging.INFO,
-    handlers=[logging.StreamHandler(), logging.FileHandler("proxy_multiplex.log")],
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler("proxy_multiplex_8192.log"),
+    ],
 )
 logger = logging.getLogger(__name__)
 
@@ -34,10 +37,10 @@ class ResponseType:
 
 class NtoMProxy:
     def __init__(self, target_wss: List[str], port: int):
-        self.persistent_sessions = [None] * len(target_wss)
-        self.persistent_wss = [None] * len(target_wss)
         self.target_wss = target_wss
         self.port = port
+        self.persistent_session = None
+        self.persistent_wss = [None] * len(target_wss)
         self.serializer = Serializer()
 
         @asynccontextmanager
@@ -48,30 +51,37 @@ class NtoMProxy:
                 asyncio.create_task(self.response_consumer(tsid))
             yield
             await self.close_connection()
-            
 
         self.app = FastAPI(lifespan=lifespan)
         self.app.websocket("/")(self.connect)
 
         self.gids = [0] * len(self.target_wss)
 
-        self.gid_tsid_to_lid_wsid = (
+        self.gid_tsid_to_lid_wsid: Dict[Tuple[int, int], Tuple[int, int]] = (
             {}
         )  # (global id, target socket id) -> (local id, wsid)
         self.wsid_to_guids = defaultdict(set)
-        self.guid_to_wsid = {}
-        self.wsid_to_ws = {}
-        self.wsid_to_tsid = {}  # assignment of a client to a target socket
+        self.guid_to_wsid: Dict[str, int] = {}
+        self.wsid_to_ws: Dict[int, WebSocket] = {}
+        self.wsid_to_tsid: Dict[int, int] = (
+            {}
+        )  # assignment of a client to a target socket
         self.tsid_active_count = [0] * len(self.target_wss)
 
-        self.request_queue = asyncio.Queue()
-        self.unresolved_queue = [asyncio.Queue() for _ in range(len(self.target_wss))]
+        self.request_queue: asyncio.Queue[Tuple[int, int, dict]] = asyncio.Queue()
+
+        self.wait_for_trace_queue: List[asyncio.Queue[Tuple[int, dict]]] = [
+            asyncio.Queue() for _ in range(len(self.target_wss))
+        ]
+        self.wait_for_artifact_queue: List[asyncio.Queue[Tuple[int, dict]]] = [
+            asyncio.Queue() for _ in range(len(self.target_wss))
+        ]
 
         self.initialize_msg_reply = [
             [] for _ in range(len(self.target_wss))
         ]  # get this from target_ws
 
-    async def assign(self, wsid: str):
+    async def assign(self, wsid: int):
         least_connect_tsid = self.tsid_active_count.index(min(self.tsid_active_count))
         self.wsid_to_tsid[wsid] = least_connect_tsid
         self.tsid_active_count[least_connect_tsid] += 1
@@ -79,9 +89,8 @@ class NtoMProxy:
         return least_connect_tsid
 
     async def start_connection(self):
-        for i in range(len(self.target_wss)):
-            if self.persistent_sessions[i] is None:
-                self.persistent_sessions[i] = aiohttp.ClientSession()
+        if self.persistent_session is None:
+            self.persistent_session = aiohttp.ClientSession()
 
         header = {
             "user-agent": "Playwright/1.51.1 (x64; ubuntu 24.04) python/3.11",
@@ -98,9 +107,7 @@ class NtoMProxy:
         }
 
         self.persistent_wss = [
-            await self.persistent_sessions[i].ws_connect(
-                self.target_wss[i], headers=header
-            )
+            await self.persistent_session.ws_connect(self.target_wss[i], headers=header)
             for i in range(len(self.target_wss))
         ]
 
@@ -126,45 +133,39 @@ class NtoMProxy:
                     }
                 )
             )
-        logger.info("Sent initialize message")
-    
+        logger.debug("Sent initialize message")
 
         async def listen_to_ws(i):
-            logger.info(f"Listening to {self.target_wss[i]}")
+            logger.debug(f"Listening to {self.target_wss[i]}")
             async for msg in self.persistent_wss[i]:
-                if msg.type == aiohttp.WSMsgType.TEXT:
-                    data = self.serializer.loads(msg.data)
-                    logger.info(f"Received initialize message: {data}")
-                    if data.get("id", None) is None:
-                        self.initialize_msg_reply[i].append(msg.data)
-                    elif data.get("id", None) == self.gids[i]:
-                        self.initialize_msg_reply[i].append(msg.data)
-                        break
-                    else:
-                        raise ValueError(f"Unknown message: {msg.data}")
-                else:
+                if msg.type != aiohttp.WSMsgType.TEXT:
                     raise ValueError(f"Unknown message type: {msg.type}")
+
+                data = self.serializer.loads(msg.data)
+                msg_id = data.get("id", None)
+                if msg_id is None or msg_id == self.gids[i]:
+                    self.initialize_msg_reply[i].append(msg.data)
+                    if msg_id == self.gids[i]:
+                        break
+                else:
+                    raise ValueError(f"Unknown message: {msg.data}")
+            logger.debug(f"Received all initialize messages from {i}")
 
         await asyncio.gather(*[listen_to_ws(i) for i in range(len(self.target_wss))])
 
     async def close_connection(self):
-        tasks = [
-            self.persistent_sessions[i].close()
-            for i in range(len(self.target_wss))
-        ]
-        await asyncio.gather(*tasks)
-
         tasks = [
             ws.close()
             for ws in self.wsid_to_ws.values()
             if ws.client_state == WebSocketState.CONNECTED
         ]
         await asyncio.gather(*tasks)
+        await self.persistent_session.close()
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
 
-        wsid = str(id(websocket))
+        wsid = id(websocket)
         self.wsid_to_ws[wsid] = websocket
         tsid = await self.assign(wsid)
 
@@ -188,11 +189,12 @@ class NtoMProxy:
                     else:
                         raise ValueError(f"Unknown message type: {msg}")
         except WebSocketDisconnect:
-            logger.info(f"Client disconnected for wsid: {wsid}")
+            logger.debug(f"Client disconnected for wsid: {wsid}")
         except Exception as e:
             logger.error(f"Error in connect: {e}")
             raise e
         finally:
+            logger.debug(f"Client disconnected for wsid: {wsid}")
             self.wsid_to_ws.pop(wsid)
             self.tsid_active_count[tsid] -= 1
             assert self.tsid_active_count[tsid] >= 0
@@ -206,32 +208,34 @@ class NtoMProxy:
         if data.get("method", None) == "initialize":
             local_id = data["id"]
             ws = self.wsid_to_ws[wsid]
-            logger.debug(f"BACKWARD: {self.initialize_msg_reply[tsid][:-1]}")
             for msg in self.initialize_msg_reply[tsid][:-1]:
                 await ws.send_text(msg)
 
             reply = self.serializer.loads(self.initialize_msg_reply[tsid][-1])
             reply["id"] = local_id
-            logger.debug(f"BACKWARD: {reply}")
             await ws.send_text(self.serializer.dumps(reply))
             return
 
         self.gids[tsid] += 1
+        gid = self.gids[tsid]
         assert data.get("id", None) is not None
-        self.gid_tsid_to_lid_wsid[(self.gids[tsid], tsid)] = (data["id"], wsid)
+        self.gid_tsid_to_lid_wsid[(gid, tsid)] = (data["id"], wsid)
 
+        # listen for trace or artifact, they'll return new guids never seen before
         if data.get("method", None) == "newContext":
             logger.debug(f"UNRESOLVED: {data} from {wsid}")
-            await self.unresolved_queue[tsid].put((wsid, data))
+            await self.wait_for_trace_queue[tsid].put((wsid, data))
+            if data.get("params", {}).get("recordVideo", None) is not None:
+                await self.wait_for_artifact_queue[tsid].put((wsid, data))
 
         guids = []
         self.parse_guid_from_nested_data(data, guids)
         await self.update_guid(wsid, guids)
 
-        data["id"] = self.gids[tsid]
+        data["id"] = gid
         logger.debug(f"FORWARD: {data}")
         await self.persistent_wss[tsid].send_str(self.serializer.dumps(data))
-    
+
     async def handle_response(self, msg: aiohttp.WSMsgType, tsid: int):
         if msg.type == aiohttp.WSMsgType.TEXT:
             data = self.serializer.loads(msg.data)
@@ -249,13 +253,18 @@ class NtoMProxy:
             elif response_type == ResponseType.EVENT:
                 guids = []
                 self.parse_guid_from_nested_data(data, guids)
-                wsid = await self.findout_wsid_from_guids(guids, tsid)
+                wsid = await self.findout_wsid_from_guids(guids, tsid, data)
+                if wsid is None:
+                    logger.warn(f"Failed to find wsid for {data}")
+                    return
                 await self.update_guid(wsid, guids)
                 ws = self.wsid_to_ws[wsid]
                 logger.debug(f"BACKWARD: {data}")
                 await ws.send_text(msg.data)
             elif response_type == ResponseType.ERROR:
-                logger.error(f"Error: {data}")
+                logger.error(
+                    f"Error: {data}, shouldn't happen as error should come with an id."
+                )
 
     def parse_guid_from_nested_data(self, data: Union[dict, list], ls: List[str]):
         if not isinstance(data, dict) and not isinstance(data, list):
@@ -279,7 +288,7 @@ class NtoMProxy:
         else:
             raise ValueError(f"Unknown message type: {data}")
 
-    async def findout_wsid_from_guids(self, guids: List[str], tsid: int):
+    async def findout_wsid_from_guids(self, guids: List[str], tsid: int, data: dict):
         for guid in guids:
             if "browser@" in guid:
                 continue
@@ -289,13 +298,33 @@ class NtoMProxy:
                 return wsid
 
         # now its unresolved
-        assert self.unresolved_queue[tsid].qsize() > 0, f"Unresolved queue for {tsid} is empty, this should not happen"
-        wsid, _ = await asyncio.wait_for(self.unresolved_queue[tsid].get(), timeout=1)
-        logger.debug(f"FOUND by unresolved: {guids} -> {wsid}")
-        await self.update_guid(wsid, guids)
-        return wsid
+        logger.debug(
+            f"Unresolved: {data}\n"
+            f"Wait for trace queue for {tsid} is {self.wait_for_trace_queue[tsid].qsize()}\n"
+            f"Wait for artifact queue for {tsid} is {self.wait_for_artifact_queue[tsid].qsize()}"
+        )
+        if (
+            data.get("method", None) == "__create__"
+            and data.get("params", {}).get("type", None) == "Tracing"
+        ):
+            wsid, _ = await asyncio.wait_for(
+                self.wait_for_trace_queue[tsid].get(), timeout=1
+            )
+            logger.debug(f"FOUND by unresolved: {guids} -> {wsid}")
+            return wsid
+        elif (
+            data.get("method", None) == "__create__"
+            and data.get("params", {}).get("type", None) == "Artifact"
+        ):
+            wsid, _ = await asyncio.wait_for(
+                self.wait_for_artifact_queue[tsid].get(), timeout=1
+            )
+            logger.debug(f"FOUND by unresolved: {guids} -> {wsid}")
+            return wsid
+        else:
+            return None
 
-    async def update_guid(self, wsid: str, guids: List[str]):
+    async def update_guid(self, wsid: int, guids: List[str]):
         for guid in guids:
             if "browser@" in guid:
                 continue
@@ -305,11 +334,17 @@ class NtoMProxy:
     async def request_consumer(self):
         while True:
             wsid, tsid, data = await self.request_queue.get()
-            await self.handle_request(wsid, tsid, data)
+            try:
+                await self.handle_request(wsid, tsid, data)
+            except Exception as e:
+                logger.error(f"Error in request_consumer: {e}")
 
     async def response_consumer(self, tsid: int):
         async for msg in self.persistent_wss[tsid]:
-            await self.handle_response(msg, tsid)
+            try:
+                await self.handle_response(msg, tsid)
+            except Exception as e:
+                logger.error(f"Error in response_consumer: {e}")
 
 
 # uvicorn src.proxy_multiplex:app --host 0.0.0.0 --port 8000 --workers 8
@@ -328,5 +363,3 @@ targets = [
 proxy = NtoMProxy(targets, 8000)
 app = proxy.app
 app.add_middleware(CORSMiddleware, allow_origins=["*"])
-
-logger.info(f"Worker {worker_id} running on {targets}")
